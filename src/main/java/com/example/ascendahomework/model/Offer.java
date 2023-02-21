0b80995eb148bf0cc5d220adcd08dc68eacc62ae
KhanhMai <dkmai18@apcs.vn>
2023-02-21 22:00:13 +0700

    Complete the function of filtering offer

--- src/main/java/com/example/ascendahomework/model/Offer.java
+++ src/main/java/com/example/ascendahomework/model/Offer.java
@@ -6,13 +6,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
-//@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 public class Offer implements Serializable {
     @JsonProperty("id")
@@ -25,11 +24,27 @@ public class Offer implements Serializable {
     @JsonProperty("category")
     private Integer category;
     @JsonProperty("merchants")
+    @Setter
     private ArrayList<Merchant> merchants;
 
     @JsonProperty("valid_to")
     @JsonDeserialize(using = CustomDateDeserializer.class)
     private LocalDate validTo;
+
+    public Offer shallowCopy(){
+        Offer newOffer = new Offer();
+
+        newOffer.id = this.id;
+        newOffer.title = this.title;
+        newOffer.description = this.description;
+        newOffer.category = this.category;
+        newOffer.validTo = this.validTo;
+        newOffer.merchants = new ArrayList<>();
+        for (Merchant m : this.merchants){
+            newOffer.merchants.add(m);
+        }
+        return newOffer;
+    }
 
     public boolean isValidUntil(LocalDate date){
         if (validTo.compareTo(date) >= 0)
@@ -37,5 +52,20 @@ public class Offer implements Serializable {
         return false;
     }
 
+    public Merchant findClosestMerchant(){
+        if (merchants == null || merchants.size() == 0)
+            return null;
+        Float minDistance = merchants.get(0).getDistance();
+        int index = 0;
+        for (int i = 1; i< merchants.size(); ++i){
+            if (minDistance.compareTo(merchants.get(i).getDistance()) > 0) { // minDistance > merchants[i].distance
+                minDistance = merchants.get(i).getDistance();
+                index = i;
+            }
+        }
+
+        return merchants.get(index);
+
+    }
 
 }
--- src/main/java/com/example/ascendahomework/service/implement/OfferServiceImp.java
+++ src/main/java/com/example/ascendahomework/service/implement/OfferServiceImp.java
@@ -1,5 +1,6 @@
 package com.example.ascendahomework.service.implement;
 
+import com.example.ascendahomework.model.Merchant;
 import com.example.ascendahomework.model.Offer;
 import com.example.ascendahomework.service.OfferService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,9 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Slf4j
@@ -22,10 +24,10 @@ public class OfferServiceImp implements OfferService {
     private RestTemplate restTemplate = new RestTemplate();
 
     @Override
-    public ArrayList<Offer> getNearByOffer(LocalDate checkinDate) throws JsonProcessingException, ParseException {
+    public ArrayList<Offer> getNearByOffer(LocalDate checkinDate) throws JsonProcessingException{
         String url = "https://61c3deadf1af4a0017d990e7.mockapi.io/offers/near_by?lat=1.313492&lon=103.860359&rad=20";
         ArrayList<Offer> offers = fetchExternalAPI(url);
-        offers = filterOffer(checkinDate,offers);
+        offers = filterOffer(checkinDate,offers,2);
 
         return offers;
 
@@ -50,17 +52,47 @@ public class OfferServiceImp implements OfferService {
         return offerArrayList;
     }
 
-    public ArrayList<Offer> filterOffer(LocalDate checkinDate, ArrayList<Offer> offers){
-        ArrayList<Offer> result = new ArrayList<>();
+    public ArrayList<Offer> filterOffer(LocalDate checkinDate, ArrayList<Offer> offers, int numberOfOffer){
+        ArrayList<Offer> offerArrayList = new ArrayList<>();
 
         for (Offer offer: offers){
-            if (offer.isValidUntil(checkinDate.plusDays(5))){
-                System.out.println(offer.getId());
-                System.out.println(offer.getValidTo());
-                result.add(offer);
+            if (offer.isValidUntil(checkinDate.plusDays(5)) && offer.getCategory() != 3){
+
+
+                Offer newOffer = offer.shallowCopy();
+                Merchant closestMerchant = offer.findClosestMerchant();
+                ArrayList<Merchant> tmp = new ArrayList<>();
+                tmp.add(closestMerchant);
+                newOffer.setMerchants(tmp);
+
+                offerArrayList.add(newOffer);
             }
         }
 
+        offerArrayList.sort((o1, o2) -> {
+            if (o1.getCategory() == o2.getCategory()){
+                Float d1 = o1.findClosestMerchant().getDistance();
+                Float d2 = o2.findClosestMerchant().getDistance();
+                return d1.compareTo(d2);
+            }
+            else{
+                return o1.getCategory().compareTo(o2.getCategory());
+            }
+        });
+
+        Set<Integer> selectedCategory = new HashSet<>();
+        ArrayList<Offer> result = new ArrayList<>();
+        int index = 0;
+
+        while(numberOfOffer > 0 && index < offerArrayList.size()){ // number of offer may useful for the case of selecting any number of offers
+            if (!selectedCategory.contains(offerArrayList.get(index).getCategory())){
+                result.add(offerArrayList.get(index));
+                selectedCategory.add(offerArrayList.get(index).getCategory());
+                --numberOfOffer;
+            }
+            ++index;
+        }
+        
         return result;
     }
 
